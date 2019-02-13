7195b3fa3c3a539e2336ccb614ac37080bafd9db
WHQ <2283149823@qq.com>
2019-02-13 21:25:18 +0800

    Mybatis related

--- src/main/java/com/example/demo/entity/Mineral.java
+++ src/main/java/com/example/demo/entity/Mineral.java
@@ -1,5 +1,28 @@
 package com.example.demo.entity;
 
 public class Mineral {
+
+    private int id;
+
+    private String name;
+
+    private String type;
+
+    private String introduction;
+
+    private String color;
+
+    private String jieli;
+
+    private String tuqi;
+
+    private String colorganshe;
+
+    private int xiaoguang;
+
+    private int shuangjin;
+
+    private int guangxing;
+
     
 }

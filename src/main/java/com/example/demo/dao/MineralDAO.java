afd7aa4cc4e9fac72c18ba247223e9e61a80ac9a
WHQ <2283149823@qq.com>
2019-03-04 19:46:36 +0800

    DAO

--- src/main/java/com/example/demo/dao/MineralDAO.java
+++ src/main/java/com/example/demo/dao/MineralDAO.java
@@ -1,5 +1,9 @@
 package com.example.demo.dao;
 
-public class MineralDAO {
+import com.example.demo.entity.Mineral;
+
+public interface MineralDAO {
+    Mineral findbyname(String name);
+
 
 }
--- src/main/java/com/example/demo/entity/Mineral.java
+++ src/main/java/com/example/demo/entity/Mineral.java
@@ -24,5 +24,91 @@ public class Mineral {
 
     private int guangxing;
 
-    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getJieli() {
+        return jieli;
+    }
+
+    public void setJieli(String jieli) {
+        this.jieli = jieli;
+    }
+
+    public String getTuqi() {
+        return tuqi;
+    }
+
+    public void setTuqi(String tuqi) {
+        this.tuqi = tuqi;
+    }
+
+    public String getColorganshe() {
+        return colorganshe;
+    }
+
+    public void setColorganshe(String colorganshe) {
+        this.colorganshe = colorganshe;
+    }
+
+    public int getXiaoguang() {
+        return xiaoguang;
+    }
+
+    public void setXiaoguang(int xiaoguang) {
+        this.xiaoguang = xiaoguang;
+    }
+
+    public int getShuangjin() {
+        return shuangjin;
+    }
+
+    public void setShuangjin(int shuangjin) {
+        this.shuangjin = shuangjin;
+    }
+
+    public int getGuangxing() {
+        return guangxing;
+    }
+
+    public void setGuangxing(int guangxing) {
+        this.guangxing = guangxing;
+    }
 }
